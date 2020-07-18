f6d94557df2553def98d9049bc3a232ef0068e47
niuhb2 <niuhaibao@gmail.com>
2020-07-18 23:31:09 +0800

    定义post数据模型

--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """文章表"""
+    title = models.CharField(verbose_name='贴子标题', max_length=200)
+    slug = models.CharField(verbose_name='贴子摘要', max_length=200)
+    body = models.TextField(verbose_name='贴子内容')
+    pub_date = models.DateTimeField(verbose_name='贴子的发表时间', default=timezone.now)
+
+    class Meta:
+        # 模型对象返回的结果记录集是按照这个字段反向排序的
+        ordering = ('-pub_date',)
+        # 定义在admin后台显示的表名称
+        verbose_name = '贴子'
+        verbose_name_plural = '贴子'
+
+    def __str(self):
+        return self.title
