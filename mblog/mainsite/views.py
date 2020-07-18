6eaa5712544f1cacd7e8574dd5229481b25a3919
niuhb2 <niuhaibao@gmail.com>
2020-07-19 00:16:35 +0800

    view homepage

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Post
+from datetime import datetime
 
-# Create your views here.
+
+def homepage(request):
+    now = datetime.now()
+    posts = Post.objects.all()
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append('No.{}'.format(str(count)) + str(post.title) + '<br>')
+        post_lists.append('<small>' + str(post.body)
+                          + '</small><br><br>')
+    print(post_lists)
+    return HttpResponse(post_lists)
