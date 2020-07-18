d3d68f6dfdb8ee0f4b299ad68c6d16694e47802f
niuhb2 <niuhaibao@gmail.com>
2020-07-19 00:22:50 +0800

    view homepage

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, loader
 from .models import Post
 from datetime import datetime
 
@@ -6,10 +6,6 @@ from datetime import datetime
 def homepage(request):
     now = datetime.now()
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append('No.{}'.format(str(count)) + str(post.title) + '<br>')
-        post_lists.append('<small>' + str(post.body)
-                          + '</small><br><br>')
-    print(post_lists)
-    return HttpResponse(post_lists)
+    template = loader.get_template('mainsite/index.html')
+    html = template.render(locals())
+    return HttpResponse(html)
