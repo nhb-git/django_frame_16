21cd178ae1412b7fad41e97034e8d914ce619bd7
niuhb2 <niuhaibao@gmail.com>
2020-07-18 23:30:44 +0800

    注册数据模型到admin后台

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainsite.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    """Post模型在admin后台的自定义显示列"""
+    list_display = ('title', 'slug', 'pub_date')
+
+
+admin.site.register(Post, PostAdmin)
