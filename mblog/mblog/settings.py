42548d83e2c6aec33c7766e4d48d0ff28193243d
niuhb2 <niuhaibao@gmail.com>
2020-07-20 10:47:38 +0800

    install markdown app

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainsite.apps.MainsiteConfig',
+    'markdown_deux',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
