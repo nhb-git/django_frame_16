12492107884efb19c9adf19fa99dfbd1e876c471
niuhb2 <niuhaibao@gmail.com>
2020-08-09 22:10:04 +0800

    更新了数据库模型

--- ch09/app01/models.py
+++ ch09/app01/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """用户表"""
+    name = models.CharField(verbose_name='用户名', max_length=20, null=False)
+    email = models.EmailField(verbose_name='邮箱')
+    password = models.CharField(verbose_name='密码', max_length=20, null=False)
+    enabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
